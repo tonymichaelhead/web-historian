ee76b03641ba59fcd3b3ed9097c464fee60d90a5
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-20 19:20:49 -0700

    Implement http-helper functions

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var Promise = require('bluebird');
 
 exports.headers = {
   'access-control-allow-origin': '*',
@@ -10,17 +11,44 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
-exports.sendRedirect = function() {
+exports.sendRedirect = function(response, location, status) {
+  status = status || 302;
+  response.writeHead(status, {Location: location});
+};response.end();
 
-};
+var readFile = Promise.promisify(fs.readFile);
 
 exports.serveAssets = function(res, asset, callback) {
   var encoding = {encoding: 'utf-8'};
-  
+
+  return readFile(archive.paths.siteAssets + asset, encoding)
+    .then(contents => {
+      contents && exports.SendResponse(res, contents);
+    })
+    .catch(err => {
+      return readFile(archive.paths.archivedSites + asset, encoding);
+    })
+    then(contents => {
+      contents && exports.SendResponse(res, contents);
+    })
+    .catch(err => {
+      callback ? callback() : exports.send404(res);
+    });
+
 };
 exports.collectData = function(request, callback) {
+  var data = '';
+  request.on('data', chunk => {
+    data += chunk;
+  });
+  request.on('end', () => {
+    callback(data);
+  });
+};
 
-}
+exports.send404 = function(response) {
+  exports.sendResponse(response, '404: Page not found', 404);
+};
 
 
 
